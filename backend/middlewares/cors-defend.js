7c1c5a059a5b010bac32e253eb8091ee831a2f2d
Vladimir Blinov <zemetrans21@yandex.ru>
2021-08-26 17:42:37 +0300

    feat: сделать мидлвару CORS

---
+++ backend/middlewares/cors-defend.js
@@ -0,0 +1,29 @@
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+    "mestosquid.nomoredomains.rocks",
+    "localhost:3001"
+];
+
+module.exports = (req, res, next) => {
+    const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+    const DEFAULT_ALLOWED_METHODS = "GET,PATCH,POST,DELETE,PUT";
+    // проверяем, что источник запроса есть среди разрешённых
+    if (allowedCors.includes(origin)) {
+        // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    // Если это предварительный запрос, добавляем нужные заголовки
+    // сохраняем список заголовков исходного запроса
+    const requestHeaders = req.headers['access-control-request-headers'];
+    if (method === 'OPTIONS') {
+        // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+        res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+        // разрешаем кросс-доменные запросы с этими заголовками
+        res.header('Access-Control-Allow-Headers', requestHeaders);
+        // завершаем обработку запроса и возвращаем результат клиенту
+        return res.end();
+    }
+    next();
+};
